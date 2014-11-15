f0b31c4dde9f24eecc6390126c2bbf29349e70f5
ingdesoft5 <ppmiquel@uc.cl>
2014-11-15 16:16:00 -0300

    Ultimos cambios
    Subiendo los ultimos cambios al FrontEnd

---
+++ Proyecto Ingeniería de Software/src/Backend/Actor.java
@@ -0,0 +1,14 @@
+package Backend;
+
+public class Actor {
+	public String type;
+	public String id;
+	public String name;
+	
+	public Actor(String type, String id, String name){
+		this.type = type;
+		this.id = id;
+		this.name = name;
+	}
+	
+}
