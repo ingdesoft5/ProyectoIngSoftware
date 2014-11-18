b15ef366f3a611aef4e3e40f0f6006150ff098c4
ingdesoft5 <ppmiquel@uc.cl>
2014-11-17 21:12:00 -0300

    Cambio nombre Test

---
+++ Proyecto Ingeniería de Software/src/JUnitTests/TestEditorXML.java
@@ -0,0 +1,30 @@
+package JUnitTests;
+
+import static org.junit.Assert.*;
+
+import javax.swing.JTextPane;
+
+import org.junit.Test;
+
+public class TestEditorXML {
+
+	@Test
+	public void TestEscribirEditorXML() {
+		JTextPane textArea = new JTextPane();
+		String inicio = textArea.getText();
+		textArea.setText("Prueba Escribir Texto");
+		String fin = textArea.getText();
+		assertNotEquals("Prueba Escribir en Editor XML", inicio, fin);
+	}
+	
+	@Test
+	public void TestBorrarEditorXML(){
+		JTextPane textArea = new JTextPane();
+		textArea.setText("Prueba Borrar Texto");
+		String inicio = textArea.getText();
+		textArea.setText("");
+		String fin = textArea.getText();
+		assertNotEquals("Prueba Borrar en Editor XML", inicio, fin);
+	}
+
+}
