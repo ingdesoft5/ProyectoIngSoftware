633fd4d1b49bca5bdb6f822ce273613b1bd225c1
ingdesoft5 <mjrivera2@uc.cl>
2014-11-19 00:00:24 -0300

    FIN

--- Proyecto Ingeniería de Software/src/javagui/views/Conexion.java
+++ Proyecto Ingeniería de Software/src/javagui/views/Conexion.java
@@ -24,35 +24,35 @@ public class Conexion extends GroupLayout{
 		}
 		BufferedImage img = new BufferedImage(LargoX, LargoY, BufferedImage.TYPE_INT_ARGB);
 		Graphics g = img.getGraphics();
-		g.setColor(Color.black);
+		g.setColor(Color.BLACK);
 
 		if(tipo=="extend"){
 			int x = (FinX - InicioX)/10;
 			int y = (FinY - InicioY)/10;
 			for(int i=0;i<5;i++){
-				g.setColor(Color.black);
+				g.setColor(Color.BLACK);
 				g.drawLine(InicioX+x*i, InicioY+y*i, InicioX+x*(i+1), InicioY+y*(i+1));
 				i++;
 			}
-			g.setColor(Color.black);
-			g.drawLine(FinX-5, InicioY, InicioX, InicioY);
-			g.drawLine(InicioX, InicioY, InicioX, InicioY);	
+			g.setColor(Color.BLACK);
+			//g.drawLine(FinX-5, InicioY, InicioX, InicioY);
+			//g.drawLine(InicioX, InicioY, InicioX, InicioY);	
 		}
 		else if(tipo=="include"){
 			int x = (FinX - InicioX)/10;
 			int y = (FinY - InicioY)/10;
 			for(int i=0;i<5;i++){
-				g.setColor(Color.black);
+				g.setColor(Color.BLACK);
 				g.drawLine(InicioX+x*i, InicioY+y*i, InicioX+x*(i+1), InicioY+y*(i+1));
 				i++;
 			}
 		}
 		else if(tipo=="basic"){
-			g.setColor(Color.black);
+			g.setColor(Color.BLACK);
 			g.drawLine(InicioX, InicioY, FinX, FinY);
 		}
 		else if(tipo=="isa"){
-			g.setColor(Color.black);
+			g.setColor(Color.BLACK);
 			g.drawLine(InicioX, InicioY, FinX-5, FinY-5);
 			g.drawLine(FinX-5, FinY-5, FinX-5, FinY+5);
 			g.drawLine(FinX-5, FinY-5, FinX, FinY);
@@ -62,27 +62,27 @@ public class Conexion extends GroupLayout{
 			int x = (FinX - InicioX)/10;
 			int y = (FinY - InicioY)/10;
 			for(int i=0;i<5;i++){
-				g.setColor(Color.black);
+				g.setColor(Color.BLACK);
 				g.drawLine(InicioX+x*i, InicioY+y*i, InicioX+x*(i+1), InicioY+y*(i+1));
 				i++;
 			}
 		}
 		else if(tipo=="association"){
-			g.setColor(Color.black);
+			g.setColor(Color.BLACK);
 			g.drawLine(InicioX, InicioY, FinX, FinY);
 		}
 		else if(tipo=="aggregation"){
-			g.setColor(Color.black);
+			g.setColor(Color.BLACK);
 			g.drawLine(InicioX, InicioY, FinX-5, FinY-5);
 			g.drawRect(FinX-5, FinY-5, FinX, FinY);
 		}
 		else if(tipo=="composition"){
-			g.setColor(Color.black);
+			g.setColor(Color.BLACK);
 			g.drawLine(InicioX, InicioY, FinX-5, FinY-5);
 			g.drawRect(FinX-5, FinY-5, FinX, FinY);
 		}
 		else if(tipo=="inheritance"){
-			g.setColor(Color.black);
+			g.setColor(Color.BLACK);
 			g.drawLine(InicioX, InicioY, FinX-5, FinY-5);
 			g.drawLine(FinX-5, FinY-5, FinX-5, FinY+5);
 			g.drawLine(FinX-5, FinY-5, FinX, FinY);
